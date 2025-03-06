024cc092a02eba7f790765942c897fd764f04cac
Mohit yadav <mohyadav3@WKMZT529854A.global.publicisgroupe.net>
2025-03-06 20:12:20 +0530

    intial project setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+  return <h1>React project Setup
+  </h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    hot: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
